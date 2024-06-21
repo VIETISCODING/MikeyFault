de1f1d63d34bc4d300ae5c313e083197f1116bf5
VIETISCODING <148327111+VIETISCODING@users.noreply.github.com>
2024-06-21 15:18:46 +0700

    CPP0119-
    ƯỚC SỐ CHIA HẾT CHO 2

--- exercise.cpp
+++ exercise.cpp
@@ -2,21 +2,21 @@
 typedef long long ll; //using ll=long long
 using namespace std;
 
-ll ok(ll n){
-    for(ll i=2;i<=sqrt(n);i++){
-    	if(n%i==0) return i;
-	}
-	return n;
-}
 int main(){
 	ll t;
 	cin>>t;
 	while(t--){
-		ll n;
+        ll n,cnt=0;
         cin>>n;
-	    for(ll i=1;i<=n;i++){
-	    	cout<<ok(i)<<" ";
+        for(ll i=1;i<=sqrt(n);i++){
+        	if(n%i==0){
+        		if(i%2==0){
+        			cnt++;
+        			if((n/i)%2==0 && (n/i)!=i) cnt++;
+				} 
+				else if(n%2==0) cnt++;
+			} 
 		}
-		cout<<endl;
-	}
+		cout<<cnt<<endl;
+    } 
 }
