999f51da24bfd684ecdf05fd24a65b4202d3ee6a
VIETISCODING <148327111+VIETISCODING@users.noreply.github.com>
2024-06-21 20:23:15 +0700

    CPP0141- KIỂM TRA SỐ FIBONACCI

--- exercise.cpp
+++ exercise.cpp
@@ -2,21 +2,30 @@
 typedef long long ll; //using ll=long long
 using namespace std;
 
+ll fibonacci(ll n){
+	vector<ll> v;
+	v.push_back(0),v.push_back(1);
+    ll a[100];
+	a[0]=0,a[1]=1;
+	for(ll i=2;i<100;i++){
+		a[i]=a[i-1]+a[i-2];
+		v.push_back(a[i]);
+	}
+	ll ok=0;
+	for(ll x:v){
+		if(n==x) ok=1;
+	}
+	if(ok==0) return 0;
+	else return 1;
+}
 int main(){
 	ll t;
 	cin>>t;
 	while(t--){
-        ll n,cnt=0;
+		ll n;
         cin>>n;
-        for(ll i=1;i<=sqrt(n);i++){
-        	if(n%i==0){
-        		if(i%2==0){
-        			cnt++;
-        			if((n/i)%2==0 && (n/i)!=i) cnt++;
-				} 
-				else if(n%2==0) cnt++;
-			} 
-		}
-		cout<<cnt<<endl;
-    } 
+        if(fibonacci(n)) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+	}
 }
+
