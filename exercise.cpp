042b49a900d0ad7fd16a7c9a2ec1bd31405c1f91
VIETISCODING <148327111+VIETISCODING@users.noreply.github.com>
2024-06-21 20:53:30 +0700

    CPP0136-ĐẾM SỐ CÓ BA ƯỚC SỐ

--- exercise.cpp
+++ exercise.cpp
@@ -2,30 +2,23 @@
 typedef long long ll; //using ll=long long
 using namespace std;
 
-ll fibonacci(ll n){
-	vector<ll> v;
-	v.push_back(0),v.push_back(1);
-    ll a[100];
-	a[0]=0,a[1]=1;
-	for(ll i=2;i<100;i++){
-		a[i]=a[i-1]+a[i-2];
-		v.push_back(a[i]);
-	}
-	ll ok=0;
-	for(ll x:v){
-		if(n==x) ok=1;
-	}
-	if(ok==0) return 0;
-	else return 1;
-}
+ll nt(ll n){
+	for(ll i=2;i<=sqrt(n);i++){
+	    if(n%i==0) return 0; 
+	} 
+	return n>1; 
+} 
 int main(){
 	ll t;
 	cin>>t;
 	while(t--){
-		ll n;
-        cin>>n;
-        if(fibonacci(n)) cout<<"YES"<<endl;
-        else cout<<"NO"<<endl;
-	}
-}
+		ll n,i=1,cnt=0;
+		cin>>n;
+		while(i*i<=n){
+			if(nt(i)) cnt++;
+			i++; 
+		} 
+		cout<<cnt<<endl; 
+	} 
+} 
 
