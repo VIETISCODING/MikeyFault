c20b4791db4d7ddc78244d1d1d06451a09992e66
VIETISCODING <148327111+VIETISCODING@users.noreply.github.com>
2024-07-05 01:06:29 +0700

    0612

--- exercise.cpp
+++ exercise.cpp
@@ -1,28 +1,49 @@
 #include<bits/stdc++.h> 
 typedef long long ll; //using ll=long long
 using namespace std;
-
-
+ll k=1;
+class SinhVien{
+	private:
+		
+	public:
+		string msv,ten,lop,ns;
+		float gpa;
+	    friend istream& operator>>(istream& in,SinhVien& a){
+	    	cin.ignore();
+	    	getline(cin,a.ten);
+	    	cin>>a.lop>>a.ns>>a.gpa;
+	    	return in;
+		}	
+		friend ostream& operator<<(ostream& out,SinhVien a){
+            string token,s = "";
+            stringstream ss(a.ten);
+            while (ss >> token) {
+                s+=toupper(token[0]);
+                for(ll i=1;i<token.length();i++) s+=tolower(token[i]);
+                s+=" ";
+            }
+            s.erase(s.length()-1);
+            a.ten=s;
+			if(k<10) a.msv="B20DCCN00";
+			else a.msv="B20DCCN0";
+			a.msv+=to_string(k);
+			k++;
+			if(a.ns[1]=='/') a.ns.insert(0,"0");
+			if(a.ns[4]=='/') a.ns.insert(3,"0");
+			cout<<a.msv<<" "<<a.ten<<" "<<a.lop<<" "<<a.ns;
+			printf(" %.2f\n",a.gpa);
+			return out;
+		}
+};
 int main(){
-	ll t;
-	cin>>t;
-	while(t--){
-	    ll n;
-		cin>>n;
-		ll a[n];
-		for(ll i=0;i<n;i++) cin>>a[i];
-		ll ok=0,min=10e-7;
-		for(ll i=0;i<n;i++){	 
-			for(ll j=i+1;j<n;j++){
-				if(a[i]<a[j]){
-					if(min<a[j]-a[i]){
-						ok=1;
-						min=a[j]-a[i];
-					} 
-				} 
-			} 
-		} 
-		if(ok==0) cout<<"-1"<<endl;
-		else cout<<min<<endl; 
-	} 
-} 
+    SinhVien ds[50];
+    int N, i;
+    cin >> N;
+    for(i=0;i<N;i++){
+        cin >> ds[i];
+    }
+    for(i=0;i<N;i++){
+        cout << ds[i];
+    }
+    return 0;
+}
